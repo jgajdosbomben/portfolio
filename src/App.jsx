82f1a26e8a2f7c66424d89db52fc7ac886f543e9
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-08-02 11:30:51 -0600

    Added new sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Header from "./components/Header";
+import Education from "./components/Education";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -21,8 +22,8 @@ function App() {
 
   return (
     <div className={`"w-full h-full min-h-[100vh] bg-white" ${darkMode && "dark"}`}>
-      <div className="w-full h-full min-h-[100vh] bg-gradient-to-tr from-[#ddaacc] via-[#74777e] to-[#b4b7be]">
-        <div className="dark:bg-gradient-to-tr from-[#cc29b9] via-[#030a1c] to-[#05174e]">
+      <div className="w-full h-full min-h-[100vh] bg-gradient-to-r from-[#ffcccc] via-[#ffddcc] to-[#fbe9ed]">
+        <div className="dark:bg-gradient-to-tr from-[#cc29b9] via-[#030a1c] to-[#21054e]">
           <Navbar
             darkMode={darkMode}
             isOpen={isOpen}
@@ -33,6 +34,30 @@ function App() {
             id="home"
             className="px-0 lg:px-5 lg:py-0 2xl:px-40 py-10">
             <Header />
+          </section>
+          <section
+            id="about"
+            // className="w-full h-full min-h-[100vh] bg-gradient-to-r from-[#ffcccc] via-[#ffddcc] to-[#fbe9ed]">
+            className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-gradient-to-tr from-[#030a1c] via-[#941d87] via-80% to-[#21054e]">
+            <About />
+          </section>
+          <section
+            id="education"
+            // className="w-full h-full min-h-[100vh] bg-gradient-to-r from-[#ffcccc] via-[#ffddcc] to-[#fbe9ed]">
+            className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-gradient-to-tr from-[#030a1c] via-[#941d87] via-80% to-[#21054e]">
+            <Education />
+          </section>
+          <section
+            id="projects"
+            // className="w-full h-full min-h-[100vh] bg-gradient-to-r from-[#ffcccc] via-[#ffddcc] to-[#fbe9ed]">
+            className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-gradient-to-tr from-[#030a1c] via-[#941d87] via-80% to-[#21054e]">
+            <Projects />
+          </section>
+          <section
+            id="skills"
+            // className="w-full h-full min-h-[100vh] bg-gradient-to-r from-[#ffcccc] via-[#ffddcc] to-[#fbe9ed]">
+            className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-gradient-to-tr from-[#030a1c] via-[#941d87] via-80% to-[#21054e]">
+            <Skills />
           </section>
         </div>
       </div>
