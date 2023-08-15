0a0ad007c68a56705f037fd3735c28baa59dd2ba
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-08-15 13:09:21 -0600

    Changed background colour

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Education = () => {
-  return <div>
+  return <div className="bg-zinc-200">
        <h1 className="text-3xl">Education</h1>
+       <p>This is where I will put my education</p>
   
   </div>;
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,29 +3,35 @@ import { projects } from "../data";
 
 const Projects = () => {
   return (
-    <div>
-   <div className="flex justify-center items-center">
-      <h1 className="text-3xl">Projects</h1>
+    <div className="bg-black">
+      <div className="flex justify-center items-center">
+        <h1 className="text-3xl">Projects</h1>
       </div>
 
-    <div
-      data-aos="fade-down"
-      data-aos-offset="200"
-      data-aos-delay="50"
-      data-aos-duration="1000"
-      data-aos-easing="ease-in-out"
-      className="w-full flex flex-wrap grow-0 items-center justify-center gap-10 lg:gap-20 pt-10"
-      >
-          {projects.map((proj, index) => (
-        <div className="flex flex-col gap-2 max-w-md h-full items-center px-4 py-3 bg-white rounded-md">
-          <p className="text-black dark:text-white text-2xl font-bold">{proj.name}</p>
-          <a href={proj.url} className="text-black dark:text-white text-lg font-bold">{proj.url}</a>
-          <p className="text-black dark:text-whitetext-sm font-bold">{proj.description}</p>
-          <img src={proj.img} className="w-70 h-50 justify-end"/>
-        </div>
-      ))}
+      <div
+        data-aos="fade-down"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        className="w-full flex flex-wrap grow-0 items-center justify-center gap-10 lg:gap-20 pt-10">
+        {projects.map((proj, index) => (
+          <div className="flex flex-col gap-2 max-w-md h-full items-center px-4 py-3 bg-white rounded-md">
+            <p className="text-black dark:text-white text-2xl font-bold">{proj.name}</p>
+            <a
+              href={proj.url}
+              className="text-black dark:text-white text-lg font-bold">
+              {proj.url}
+            </a>
+            <p className="text-black dark:text-whitetext-sm font-bold">{proj.description}</p>
+            <img
+              src={proj.img}
+              className="w-70 h-50 justify-end"
+            />
+          </div>
+        ))}
+      </div>
     </div>
-</div>
   );
 };
 
