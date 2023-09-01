85a6f1b04b2b804d811227498b8f2ca98d8ea5ef
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-01 14:20:15 -0600

    Added grid and responsive breakpoints

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,28 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-5 grid-rows-5 bg-[#f4dddd]">
-      {/* items-center content-start justify-between py-40 */}
-      {/* <div className="flex flex-col mb-20 lg:mb-0"> */}
-      <div className="col-span-3 row-start-1 row-end-4 text-center align-middle pt-12">
-        <h1 className="text-6xl font-montserrat  text-black dark:text-white">
+    <div className="grid grid-cols-1 grid-row-3 md:grid-cols-5 md:grid-rows-5 bg-[#f4dddd]">
+         <div className="col-span-1 row-span-1 md:col-span-3 md:row-span-5 text-center align-middle pt-12">
+        <h1 className="text-6xl font-montserrat  text-black dark:text-white pt-10">
           JENNIFER <br />
           GAJDOS-BOMBEN
         </h1>
+        <br />
         <h2 className="text-4xl text-black text-center font-montserrat  py-12">Full Stack Developer</h2>
-      </div>
-      <div className="col-span-2 row-span-5">
-        <img
-          src="/src/assets/websitephoto.png"
-          alt="personal-image"
-          className="object-contain w-full h-full placeholder:-top-5 md:-top-10"></img>
-      </div>
-      <div className="col-span-3 text-center">
+        <br />
         <button className="rounded-sm bg-[#343434] hover:bg-pink-200 border-solid border-2 border-black font-montserrat  text-white text-xl py-2 px-8">
           Contact Me
         </button>
       </div>
+      <div className="col-span-1 row-span- md:col-span-2 md:row-span-5">
+        <img
+          src="/src/assets/websitephoto.png"
+          alt="personal-image"
+          className="object-cover w-full h-full placeholder:-top-5 md:-top-10"></img>
+      </div>
+      {/* <div className="col-span-1 md:col-span-3 text-center">
+       
+      </div> */}
 
       {/* <h1 className="text-3xl dark:text-white">
             I am a <br />
