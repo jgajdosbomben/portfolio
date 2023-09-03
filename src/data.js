6994d3be67e601b55ddcb68e063928b80032a9f9
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-02 21:45:58 -0600

    Edited unstuck project url

--- src/data.js
+++ src/data.js
@@ -3,14 +3,15 @@ export const projects = [
     id: 1,
     name: "YYC Local Art",
     url: "https://yyclocal.art",
-    description: "A website to increase awareness of local public art. An interactive map plots the locations of public art in Calgary and allows users to create custom, self-guided walking tours. ",
+    description:
+      "A website to increase awareness of local public art. An interactive map plots the locations of public art in Calgary and allows users to create custom, self-guided walking tours. ",
     img: "./src/assets/yyclocalart.png",
   },
 
   {
     id: 2,
     name: "Unstuck",
-    url: "Check out on GitHub",
+    url: "GitHub Repo: https://github.com/Unstuck-IU/unstuck",
     description: "A web application to help students learn how to easily break down problems using critical thinking skills.",
     img: "./src/assets/unstuck.png",
   },
