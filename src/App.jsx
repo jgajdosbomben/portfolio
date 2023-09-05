1e5455067480537f0ada04f26fcadd770d37f799
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-05 11:09:16 -0600

    Adjusted background

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,10 @@ function App() {
   };
 
   return (
-    <div className={`"w-full h-full min-h-[100vh] bg-[#f4dddd]" ${darkMode && "dark"}`}>
+    <div className={`"w-full h-full min-h-[100vh] bg-gradient-to-br from-[#f4dddd]  to-[#9d9b9b]" ${darkMode && "dark"}`}>
       {/* Sets main colour for light mode */}
-      <div className="w-full h-full min-h-[100vh] bg-gradient-to-br from-[#f4dddd]  to-[#9d9b9b]">
+      <div className="w-full h-full min-h-[100vh] bg-[#f4dddd]">
+      {/* gradient-to-br from-[#f4dddd]  to-[#9d9b9b] */}
         {/* [radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#f4dddd] to-[#06308b] */}
         {/* Sets main colour for dark mode */}
         <div className="dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#620646] to-[#1d1d1d]">
@@ -37,12 +38,12 @@ function App() {
           />
           <section
             id="home"
-            className="px-0 lg:py-0 2xl: py-10">
+            className="px-0 lg:py-0 2xl:py-10">
             <Header />
           </section>
           <section
             id="about"
-            className="w-full h-full min-h-[100vh] 2xl: py-10 lg:py-0 bg-black dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#28031e] to-[#1d1d1d]">
+            className="w-full h-full min-h-[100vh] 2xl: py-10 lg:py-0 bg-[#2e2c2e] dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#28031e] to-[#1d1d1d]">
             <About />
           </section>
           <section
