474fc04cdb54344989a8a8f8982f0cb45ea140ed
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-06 13:32:42 -0600

    Added click on project to redirect to url

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { projects } from "../data";
 
 const Projects = () => {
@@ -17,17 +17,22 @@ const Projects = () => {
         className="grid grid-cols-2 gap-2 items-center justify-center p-10 lg:grid-cols-4 md:gap-4 pt-10">
         {projects.map((proj, index) => (
           <div className="grid col-span-2 gap-2 h-full items-center px-4 py-3 rounded-md bg-[#f4dddd]">
-            <p className="text-black dark:text-black text-2xl font-bold">{proj.name}</p>
-            <a
-              href={proj.url}
-              className="text-black dark:text-black text-lg font-bold">
-              {proj.url}
-            </a>
-            <p className="text-black dark:text-black text-sm font-bold">{proj.description}</p>
-            <img
-              src={proj.img}
-              className="w-70 h-50 justify-end"
-            />
+            <button
+              onClick={() => {
+                window.open(proj.url, "_blank");
+              }}>
+              <p className="text-black dark:text-black text-2xl font-bold">{proj.name}</p>
+              <a
+                href={proj.url}
+                className="text-black dark:text-black text-lg hover:underline decoration-wavy">
+                {proj.url}
+              </a>
+              <p className="text-black dark:text-black text-sm">{proj.description}</p>
+              <img
+                src={proj.img}
+                className="w-70 h-50 justify-end"
+              />
+            </button>
           </div>
         ))}
       </div>
