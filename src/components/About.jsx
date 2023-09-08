bb63fdf087e81e2904c57f35bed68e9cf2734ee7
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-08 12:36:46 -0600

    Completed about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,64 +2,32 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about w-full px-10 py-10 flex flex-col justify-center">
-      <div className="w-full flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold text-white dark:text-white  ">About Me</h1>
-
-        <div className="flex">
-          {/* <div className="profile">
-            <img
-              src="/src/assets/jennifer.jpg"
-              alt="personal-image"
-              className="profile_image"></img> */}
-
-          {/* <img src="/src/assets/jenny.jpeg"
-        className="h-[290px] p-1 rounded-md ease-in-out duration-300 hover:scale-125"/> */}
-          {/* </div> */}
-          <div className="px-10 py-10">
-            <p className="text-xl text-white dark:text-white leading-8">
-              After years of searching for a career where I could balance my creative and logical/mathematical sides, I have
-              finally found a balance in full stack development. As a new graduate from InceptionU’s Full Stack Developer program,
-              I am eager to build intuitive, functional, and beautiful applications.{" "}
-            </p>
-          </div>
+    <div className="w-full px-10 py-10">
+      <h1 className="text-3xl font-bold text-white text-center dark:text-white pb-10 ">About Me</h1>
+      <div className="w-full grid grid-cols-1 grid-row-3 gap-10 md:grid-cols-6 md:grid-rows-2 justify-center items-center">
+        <div className="col-span-1 row-span-1 sm:col-span-2 md:row-span-2">
+          <img
+            src="../websitephoto.png"
+            alt="personal-image"
+            className="object-cover w-100% h-100% placeholder:-top-5 md:-top-10 rounded-full"></img>
         </div>
-        {/* <div className="outer-headings dark:text-white">
-                    <h1>
-                      
-                      <div className="inner-headings dark:text-white">
-                        <span className="text-black dark:text-white">
-                          Full Stack Developer <br />
-                          Front End Developer <br />
-                          Back End Developer <br />
-                          Record and Information Manager <br />
-                          Dancer <br />
-                          Mother <br />
-                          Singer <br />
-                          Pianist <br />
-                          </span>
-                      </div>
-                    </h1>
-                  </div> */}
+        <div className="col-span-1 row-span-1 md:col-start-3 md:col-end-7 md:row-span-2 self-center justify-start">
+          <p className="text-xl text-white dark:text-white leading-8">
+            After years of searching for a career where I could combine my creative and logical/mathematical sides, I have finally
+            found an opportunity in full stack development. I recently pivoted from my thirteen year career in records and
+            information management and as a new graduate from InceptionU’s Full Stack Developer program, I am eager to build
+            intuitive, functional, and beautiful applications that are user-friendly and can enhance people's daily lives.{" "}
+          </p>
+          <br />
+          <p className="text-xl text-white dark:text-white leading-8">
+            Drawing from my background and love of mathematics, I enjoy using my problem solving and analysis abilities to help
+            deliver business solutions that increase efficiency and improve operations. I am extremely self-disciplined and
+            utilize my attention to detail, and communication skills to consistently deliver outstanding results.
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default About;
-
-{
-  /* <section className="diagonal">
-  <div className="wrapper">
-    <h2 className="section-title"></h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-  </div>
-</section>; */
-}
-//  <section className="diagonal">
-//     <div className="wrapper">
-
-//       <h2 className="section-title">Diagonal</h2>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque cum, perspiciatis a illo veniam ratione expedita assumenda laboriosam modi error maiores fugit soluta vitae temporibus voluptatum ducimus culpa similique quaerat?</p>
-//       </div>
-//       </section>
