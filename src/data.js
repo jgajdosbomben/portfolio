aeee2f4460e31be5cbfafe890d26ffe61de86cd3
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-08 10:38:22 -0600

    Moved images to public folder for visibility

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     url: "https://yyclocal.art",
     description:
       "A website to increase awareness of local public art. An interactive map plots the locations of public art in Calgary and allows users to create custom, self-guided walking tours. ",
-    img: "./src/assets/yyclocalart.png",
+    img: "../yyclocalart.png",
   },
 
   {
@@ -13,6 +13,6 @@ export const projects = [
     name: "Unstuck",
     url: "https://github.com/Unstuck-IU/unstuck",
     description: "A web application to help students learn how to easily break down problems using critical thinking skills.",
-    img: "./src/assets/unstuck.png",
+    img: "../unstuck.png",
   },
 ];
