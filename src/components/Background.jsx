9d6692e75a7e6fbed2d2b3a0a180724f7497f54d
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-09 21:48:54 -0600

    Alignment changes

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,17 +6,17 @@ const Background = () => {
     <div className="w-full h-full min-h-[100vh]">
       <h1 className="text-black dark:text-white text-3xl text-center font-bold px-10 py-10">Background</h1>
       <div className="flex flex-wrap justify-around pb-10">
-        <div className="inline-grid grid-cols-1 gap-2 items-center">
+        <div className="inline-grid grid-cols-1 grid-rows-6 gap-4 items-center">
           <h2 className="inline text-black dark:text-white text-xl text-center font-bold">Education</h2>
           {education.map((edu, index) => (
-            <div className="items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#e3e0e0] duration-300 px-4 py-3 justify-center rounded-md border-2 border-black bg-[#f4dddd]">
+            <div className="items-center row-span-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#e3e0e0] duration-300 px-4 py-3 justify-center self-center rounded-md border-2 border-black bg-[#f4dddd]">
               <p className="text-black dark:text-black font-bold text-base">{edu.title}</p>
               <p className="text-black dark:text-black text-base">{edu.date}</p>
               <p className="text-black dark:text-black text-base">{edu.location}</p>
             </div>
           ))}
         </div>
-        <div className="inline-grid grid-cols-1 gap-4 items-center">
+        <div className="inline-grid grid-cols-1 grid-rows-6 gap-4 items-center">
           <h2 className="inline text-black dark:text-white text-xl text-center font-bold">Experience</h2>
           {work.map((work, index) => (
             <div className="items-center items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#e3e0e0] duration-300 px-4 py-3 rounded-md border-2 border-black bg-[#f4dddd]">
