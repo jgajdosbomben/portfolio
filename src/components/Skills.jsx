2c7e054180da1390d8bafde2fdffcc2588aba774
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-10 21:29:19 -0600

    Restored blank section for now

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Skills = () => {
-  return <div className="text-black dark:text-white text-3xl text-center font-bold ppx-10 py-10">Skills</div>;
-};
+  return (
+    <div>
+      <h1 className="text-black dark:text-white text-3xl text-center font-bold px-10 py-10">Skills</h1>
 
+     </div>
+  )
+}
 export default Skills;
