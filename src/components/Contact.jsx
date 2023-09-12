54b0f21354b1a8ac22c4535b678f94c6975dbad6
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-12 14:41:39 -0600

    Added content to create form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useState, useRef } from "react";
 
 const Contact = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const formRef = useRef();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
   return (
-    <div className="bg-[#343434] dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#28031e] to-[#1d1d1d] font-montserrat text-white dark:text-white font-bold px-10 py-10">
+    <div className="text-white dark:text-white font-bold p-10">
+      {/* dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#28031e] to-[#1d1d1d]  */}
       {/* className="bg-[url('/src/assets/banner-laptop.png')]"> */}
       <h1 className="text-5xl bg-clip-text text-transparent font-bold bg-gradient-to-r from-rose-400 to-purple-800 dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-r from-rose-300 to-purple-800 text-center">
         Contact
       </h1>
-      <br />
     </div>
   );
 };
