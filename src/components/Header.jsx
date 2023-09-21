16ac340445874d72bda4de8d90910b30ef5c0ff2
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-20 21:08:25 -0600

    Font sizes increased

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ const Header = ({ isOpen, toggleMenu, toggleTheme }) => {
   return (
     <div className="main-area w-full h-full">
       <div className="header dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#620646] to-[#1d1d1d]">
-        <div className="flex flex-wrap gap-5 text-6xl text-center justify-center items-center">
+        <div className="flex flex-wrap gap-5 justify-center items-center">
           {/* bg-[#f4dddd] dark:bg-gradient-to-br from-[#1d1d1d]  to-[#9b0b71] */}
-          <div className="pt-40 pb-10 min-w-full text-2xl sm:text-6xl font-medium dark:text-white font-lobster text-center text-[#2e2c2e]">
+          <div className="pt-40 pb-10 min-w-full text-5xl sm:text-7xl font-medium dark:text-white font-lobster text-center text-[#2e2c2e]">
             Jennifer Gajdos-Bomben
           </div>
           {/* <div className="flex flex-wrap"> */}
-          <div className="p-5 min-w-full text-base sm:text-2xl text-black  dark:text-white font-montserrat text-center ">
+          <div className="p-5 min-w-full text-xl sm:text-2xl text-black  dark:text-white font-montserrat text-center ">
             Full Stack Developer | Records and Information Manager
           </div>
-          <div className="p-5 min-w-full text-base text-md sm:text-lg text-black  dark:text-white text-center">
+          <div className="p-5 min-w-full text-md sm:text-lg text-black  dark:text-white text-center">
             Passionate about blending creativity with technical solutions.
           </div>
           {/* </div> */}
