4584cf3f919cb18546a9b8f78260d739d5cbf78d
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-21 11:58:31 -0600

    Completed firebase provider

--- src/providers/FirebaseProvider.jsx
+++ src/providers/FirebaseProvider.jsx
@@ -1,19 +1,21 @@
 import React, { createContext, useContext } from "react";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv";
 
+dotenv.config();
 const FirebaseContext = createContext();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-30hpxWisEwNVrvTJKlfjYWpi0zbVyPY",
-  authDomain: "jennifergajdos-bomben.firebaseapp.com",
-  projectId: "jennifergajdos-bomben",
-  storageBucket: "jennifergajdos-bomben.appspot.com",
-  messagingSenderId: "175120246560",
-  appId: "1:175120246560:web:6bd449ec3766f365df9945",
-  measurementId: "G-JD20LRSBQ8",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_I,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export const useFirebase = () => useContext(FirebaseContext);
@@ -22,8 +24,8 @@ const FirebaseProvider = (props) => {
   const { children } = props;
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const theValues = {};
+  //   const analytics = getAnalytics(app);
+  const theValues = { app };
 
   return <FirebaseContext.Provider value={theValues}>{children}</FirebaseContext.Provider>;
 };
