65a4c86913493046852113efd47ea4f5050accf0
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-22 21:38:00 -0600

    Set up emailjs. Commented out firestore.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,30 +2,51 @@ import React from "react";
 import { useState, useRef } from "react";
 import { db } from "../Firebase";
 import { addDoc, collection } from "firebase/firestore";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const submitHandler = async (e) => {
-    console.log("Data", name, email, message);
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    try {
-      const docRef = await addDoc(collection(db, "contact_info"), {
-        name: name,
-        email: email,
-        message: message,
-      });
-      console.log("document written with ID: ", docRef.id);
-    } catch (error) {
-      console.log("error adding document: ", error);
-    }
+    // Email js setup
+    emailjs.sendForm("service_wj77ttq", "template_9z931e2", form.current, "Fkc8Tki7G1b7GXpY2").then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+    e.target.reset();
     setName("");
     setEmail("");
     setMessage("");
   };
+
+  // const submitHandler = async (e) => {
+  //   console.log("Data", name, email, message);
+  //   e.preventDefault();
+
+  //   try {
+  //     const docRef = await addDoc(collection(db, "contact_info"), {
+  //       name: name,
+  //       email: email,
+  //       message: message,
+  //     });
+  //     console.log("document written with ID: ", docRef.id);
+  //   } catch (error) {
+  //     console.log("error adding document: ", error);
+  //   }
+  //   setName("");
+  //   setEmail("");
+  //   setMessage("");
+  // };
 
   return (
     <div className="p-10">
@@ -36,7 +57,8 @@ const Contact = () => {
         <div className="w-full max-w-xl">
           <form
             className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            onSubmit={submitHandler}>
+            onSubmit={sendEmail}
+            ref={form}>
             <div className="mb-4">
               <label
                 className="block text-gray-700 dark:text-white text-sm font-bold mb-2"
@@ -48,6 +70,7 @@ const Contact = () => {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 type="text"
+                name="user_name"
                 placeholder="Name"
               />
               <br />
@@ -62,6 +85,7 @@ const Contact = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 type="email"
+                name="user_email"
                 placeholder="Email"
               />
               <br />
@@ -75,13 +99,15 @@ const Contact = () => {
                 className="shadow appearance-none border rounded w-full px-3 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 value={message}
                 placeholder="Message"
+                name="message"
                 rows="6"
                 onChange={(e) => setMessage(e.target.value)}></textarea>
               <br />
               <br />
               <button
                 className="bg-[#6a6365] hover:bg-[#620646] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit">
+                type="submit"
+                value="Send">
                 Submit
               </button>
             </div>
