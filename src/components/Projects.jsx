5c9891cc249f00efa429b6d57b4164640c0e1b5e
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-25 11:14:36 -0600

    Added alt text for project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,9 +5,7 @@ const Projects = (props) => {
   return (
     <div className="bg-[#343434] dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#28031e] to-[#1d1d1d] font-montserrat text-white dark:text-white px-1 p-10">
       <div className="flex justify-center items-center">
-        <h1 className="text-3xl sm:text-5xl text-white font-bold text-center pb-10">
-          Projects
-        </h1>
+        <h1 className="text-3xl sm:text-5xl text-white font-bold text-center pb-10">Projects</h1>
       </div>
 
       <div className="grid grid-cols-2 gap-5 items-center justify-center p-10 lg:grid-cols-4 md:gap-4 pt-10">
@@ -35,6 +33,7 @@ const Projects = (props) => {
                 }}>
                 <img
                   src={proj.img}
+                  alt="project image"
                   className="object-contain justify-end border-4 rounded-md border-[#e1aabb]/75 dark:border-[#f4dddd]/75"
                 />
               </button>
