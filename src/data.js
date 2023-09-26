a0e98da73c5858bb1cc70e8bf13fb3fff712a799
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-26 14:46:38 -0600

    Changed tech data to an array

--- src/data.js
+++ src/data.js
@@ -6,14 +6,7 @@ export const projects = [
     description:
       "A website to increase awareness of local public art. An interactive map plots the locations of public art in Calgary and allows users to create custom, self-guided walking tours. ",
     img: "../yyclocalart.png",
-    tech1: "JavaScript",
-    tech2: "HTML",
-    tech3: "CSS",
-    tech4: "Node.js",
-    tech5: "Express.js",
-    tech6: "React",
-    tech7: "Material UI",
-    tech8: "MapBox",
+    tech: ["JavaScript", "HTML", "CSS", "Node.js", "Express.js", "React", "Material UI", "MapBox"],
   },
   {
     id: 2,
@@ -21,75 +14,68 @@ export const projects = [
     url: "https://github.com/Unstuck-IU/unstuck",
     description: "A web application to help students learn how to easily break down problems using critical thinking skills.",
     img: "../unstuck.png",
-    tech1: "JavaScript",
-    tech2: "HTML",
-    tech3: "CSS",
-    tech4: "Node.js",
-    tech5: "React",
-    tech6: "Supabase",
-    tech7: "Material UI",
-    tech8: "Figma",
+    tech: ["JavaScript", "HTML", "CSS", "Node.js", "React", "Supabase", "Material UI", "Figma"],
   },
 ];
 
-export const education = [
-  {
-    id: 1,
-    title: "Full Stack Developer Program",
-    date: "Feb 2023 - Jul 2023",
-    location: "InceptionU",
-  },
+// export const education = [
+//   {
+//     id: 31,
+//     title: "Full Stack Developer Program",
+//     date: "Feb 2023 - Jul 2023",
+//     location: "InceptionU",
+//   },
 
-  {
-    id: 2,
-    title: "Certificate Holder - Essentials of RIM",
-    date: "2013",
-    location: "ARMA International",
-  },
+//   {
+//     id: 32,
+//     title: "Certificate Holder - Essentials of RIM",
+//     date: "2013",
+//     location: "ARMA International",
+//   },
 
-  {
-    id: 3,
-    title: "Bachelor of Science - Applied Mathematics, Dance Minor",
-    date: "June 2008",
-    location: "University of Calgary",
-  },
-];
+//   {
+//     id: 3,
+//     title: "Bachelor of Science - Applied Mathematics, Dance Minor",
+//     date: "June 2008",
+//     location: "University of Calgary",
+//   },
+// ];
 
-export const work = [
-  {
-    id: 1,
-    title: "Medical Legal Records Technician",
-    date: "Apr 2016 - Feb 2023",
-    location: "Office of the Chief Medical Examiner",
-  },
-  {
-    id: 2,
-    title: "Records Analyst",
-    date: "Aug 2011 - Jan 2016",
-    location: "ARC Resources Ltd.",
-  },
-  {
-    id: 3,
-    title: "Records and Information Technician",
-    date: "Feb 2011 - Aug 2011",
-    location: "ARC Resources Ltd.",
-  },
-  {
-    id: 4,
-    title: "Records and Information Technician",
-    date: "May 2010 - Dec 2010",
-    location: "DMA Canada Ltd. - Contract executed at Pengrowth Energy Corp.",
-  },
-  {
-    id: 5,
-    title: "Customer Service Representative",
-    date: "Dec 2009 - Apr 2010",
-    location: "BMO Bank of Montreal",
-  },
-  {
-    id: 6,
-    title: "Dance Teacher",
-    date: "Sep 2006 - Sep 2007",
-    location: "Airborne Dance Studio",
-  },
-];
+// export const work = [
+//   {
+//     id: 231,
+//     title: "Medical Legal Records Technician",
+//     date: "Apr 2016 - Feb 2023",
+//     location: "Office of the Chief Medical Examiner",
+//   },
+//   {
+//     id: 232,
+//     title: "Records Analyst",
+//     date: "Aug 2011 - Jan 2016",
+//     location: "ARC Resources Ltd.",
+//   },
+//   {
+//     id: 233,
+//     title: "Records and Information Technician",
+//     date: "Feb 2011 - Aug 2011",
+//     location: "ARC Resources Ltd.",
+//   },
+//   {
+//     id: 234,
+//     title: "Records and Information Technician",
+//     date: "May 2010 - Dec 2010",
+//     location: "DMA Canada Ltd. - Contract executed at Pengrowth Energy Corp.",
+//   },
+//   {
+//     id: 235,
+//     title: "Customer Service Representative",
+//     date: "Dec 2009 - Apr 2010",
+//     location: "BMO Bank of Montreal",
+//   },
+//   {
+//     id: 236,
+//     title: "Dance Teacher",
+//     date: "Sep 2006 - Sep 2007",
+//     location: "Airborne Dance Studio",
+//   },
+// ];
