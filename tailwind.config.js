73806f99069e8195d8eace3790ddc7a0274987bb
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-28 14:20:55 -0600

    Added config for material tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   presets: [],
   darkMode: "class", // or 'class' or "media"
   theme: {
@@ -989,4 +997,4 @@ export default {
     },
   },
   plugins: [],
-};
+});
