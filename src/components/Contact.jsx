d328e5b62a546f75a2fa119a7a05f541dbc552d4
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-09-28 14:33:51 -0600

    Added comments and re-arranged code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,6 +13,7 @@ const Contact = () => {
 
   const form = useRef();
 
+  //Handle submit function
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -37,6 +38,24 @@ const Contact = () => {
   const handleCloseAlert = () => {
     setShowAlert(false);
   };
+
+  function Icon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="white"
+        className="h-6 w-6">
+        <path
+          fillRule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+          clipRule="evenodd"
+        />
+      </svg>
+    );
+  }
+
+  //Firebase/Firestore connection
   // const submitHandler = async (e) => {
   //   console.log("Data", name, email, message);
   //   e.preventDefault();
@@ -55,21 +74,6 @@ const Contact = () => {
   //   setEmail("");
   //   setMessage("");
   // };
-  function Icon() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="white"
-        className="h-6 w-6">
-        <path
-          fillRule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-          clipRule="evenodd"
-        />
-      </svg>
-    );
-  }
 
   return (
     <div className="p-10">
